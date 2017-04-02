58a6068d20e2976387f3fdc1df9a66101031f945
Jackson <jromie0924@gmail.com>
2017-04-02 12:24:46 -0400

    renamed Battleship class to BattleshipInfo

--- src/battleship/backend/BattleshipInfo.java
+++ src/battleship/backend/BattleshipInfo.java
@@ -2,14 +2,14 @@ package battleship.backend;
 
 import battleship.Space;
 
-public class Battleship extends ShipInfo {
+public class BattleshipInfo extends ShipInfo {
 
 	@Override
 	public void place(int row, int col) {
 		// TODO Auto-generated method stub
 	}
 
-	public Battleship() {
+	public BattleshipInfo() {
 		size = 4;
 		damage = new int[size];
 		coordinates = new Space[size];
