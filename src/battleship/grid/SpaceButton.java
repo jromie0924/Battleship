2c6c03d0943c3866f918c355d3a2ca62cceb0282
Jackson <jromie0924@gmail.com>
2017-04-03 11:10:03 -0400

    Added SpaceButton extension of JButton to allow more customization of grid squares (which are JButtons).

---
+++ src/battleship/grid/SpaceButton.java
@@ -0,0 +1,41 @@
+package battleship.grid;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+
+public class SpaceButton extends JButton {
+	
+	private Color pressedColor;
+	private Color hoverBorderColor;
+	private Color backgroundColor;
+	
+	public SpaceButton() {
+		this(null);
+	}
+	
+	public SpaceButton(String text) {
+		super(text);
+		super.setContentAreaFilled(false);
+		backgroundColor = new Color(20, 90, 204);
+		hoverBorderColor = new Color(255, 81, 81);
+		pressedColor = backgroundColor;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		if(getModel().isPressed()) {
+			g.setColor(pressedColor);
+		
+		} else if(getModel().isRollover()) {
+			g.setColor(hoverBorderColor);
+		
+		} else {
+			g.setColor(backgroundColor);
+		}
+		
+		g.fillRect(0, 0, getWidth(), getHeight());
+		super.paintComponent(g);
+	}
+}
