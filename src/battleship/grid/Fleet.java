26e2683c9c6081f62f189754f09a28455a7f65bf
Jackson <jromie0924@gmail.com>
2017-04-03 11:10:46 -0400

    Now using SpaceButton

--- src/battleship/grid/Fleet.java
+++ src/battleship/grid/Fleet.java
@@ -3,24 +3,29 @@ package battleship.grid;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Fleet {
 	private JFrame frame;
-	private JButton[][] grid;
+	private SpaceButton[][] grid;
 	
 	public Fleet(int rows, int cols) {
 		frame = new JFrame();
 		frame.setLayout(new GridLayout(rows, cols));
-		grid = new JButton[rows][cols];
+		grid = new SpaceButton[rows][cols];
 		for(int r = 0; r < rows; r++) {
 			for(int c = 0; c < cols; c++) {
-				grid[r][c] = new JButton();
-				grid[r][c].setPreferredSize(new Dimension(60, 60));
-				grid[r][c].setBackground(new Color(20, 90, 204));
-				frame.add(grid[r][c]);
+				grid[r][c] = new SpaceButton();
+				JButton currentButton = grid[r][c];
+				currentButton.setPreferredSize(new Dimension(60, 60));
+				currentButton.setBackground(new Color(20, 90, 204));
+				currentButton.setRolloverEnabled(true);
+				
+				frame.add(currentButton);
 			}
 		}
 		
